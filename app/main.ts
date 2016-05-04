6dd190b47aca0c0dd4d7c136f9eca767479becea
Brandon Roberts <robertsbt.ags@gmail.com>
2016-05-04 01:45:28 -0600

    Step 5 - Added hash location strategy

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,12 @@ import 'rxjs/add/operator/map';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {ROUTER_PROVIDERS} from '@angular/router';
+import {provide} from '@angular/core';
+import {LocationStrategy,
+        HashLocationStrategy} from '@angular/common';
 
 bootstrap(AppComponent, [
-  ROUTER_PROVIDERS
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, {
+      useClass: HashLocationStrategy })
 ]);
